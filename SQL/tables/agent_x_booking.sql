922ad6a30a3e0f89811c399c235846b8eb6128d0
Aidan Baros <aidanlhbaros@gmail.com>
2025-11-07 12:13:23 -0600

    added setup files to run the table files

--- SQL/tables/agent_x_booking.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE
-    agent_x_booking (
-        AgentBookingID UUID PRIMARY KEY,
-        AgentID UUID REFERENCES agent (AgentID),
-        BookingID UUID REFERENCES booking (BookingID)
-    );
--- SQL/tables/booking.sql
+++ SQL/tables/booking.sql
@@ -3,6 +3,7 @@ CREATE TABLE
         BookingID UUID PRIMARY KEY,
         CardID UUID REFERENCES card (CardID),
         RenterID UUID REFERENCES renter (RenterID),
+        AgentID UUID REFERENCES agent (AgentID),
         PropertyID UUID NOT NULL,
         StartDate DATE NOT NULL,
         EndDate DATE NOT NULL
