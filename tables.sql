f3dd7a8c33d2a9039d7daefc93381da87173ebb2
Hawktseibert <tseibert@hawk.iit.edu>
2025-11-07 12:24:54 -0600

    single tables creation script

---
+++ tables.sql
@@ -0,0 +1,102 @@
+CREATE TABLE
+    users (
+        UserID UUID PRIMARY KEY,
+        Name VARCHAR(100) NOT NULL,
+        Email VARCHAR(100) UNIQUE NOT NULL,
+        Type VARCHAR(50) NOT NULL
+    );
+CREATE TABLE
+    locations (
+        LocationID UUID PRIMARY KEY,
+        Address VARCHAR(255) NOT NULL,
+        City VARCHAR(100) NOT NULL,
+        State VARCHAR(100) NOT NULL,
+        ZipCode VARCHAR(20) NOT NULL,
+        Country VARCHAR(100) NOT NULL
+    );
+CREATE TABLE
+    property (
+        PropertyID UUID PRIMARY KEY,
+        Type VARCHAR(50) NOT NULL,
+        LocationID UUID REFERENCES locations (LocationID),
+        Description TEXT NOT NULL,
+        Price DECIMAL(10, 2) NOT NULL,
+        Availability VARCHAR(50) NOT NULL,
+        CrimeRate VARCHAR(50)
+    );
+
+CREATE TABLE
+    vacationHome (
+        VacationHomeID UUID PRIMARY KEY,
+        PropertyID UUID REFERENCES property (PropertyID)
+    );
+CREATE TABLE
+    user_x_address (
+        UserAddressID UUID PRIMARY KEY,
+        UserID UUID REFERENCES users (UserID),
+        LocationID UUID REFERENCES locations (LocationID)
+    );
+
+CREATE TABLE
+    school (
+        SchoolID UUID PRIMARY KEY,
+        PropertyID UUID REFERENCES property (PropertyID),
+        Name VARCHAR(100) NOT NULL,
+        AddressID UUID REFERENCES locations (LocationID)
+    );
+CREATE TABLE
+    renter (
+        RenterID UUID PRIMARY KEY,
+        UserID UUID REFERENCES users (UserID),
+        MoveInDate DATE NOT NULL,
+        PreferedLocations VARCHAR(100),
+        Budget DECIMAL(10, 2) NOT NULL
+    );
+
+
+CREATE TABLE
+    land (
+        LandID UUID PRIMARY KEY,
+        PropertyID UUID REFERENCES property (PropertyID)
+    );
+CREATE TABLE
+    house (
+        HouseID UUID PRIMARY KEY,
+        PropertyID UUID REFERENCES property (PropertyID),
+        NumRooms INT NOT NULL,
+        SquareFeet INT NOT NULL
+    );
+CREATE TABLE
+    commercialBuilding (
+        CommercialBuildingID UUID PRIMARY KEY,
+        PropertyID UUID REFERENCES property (PropertyID),
+        SquareFeet INT NOT NULL,
+        BusinessType VARCHAR(100) NOT NULL
+    );
+CREATE TABLE
+    card (
+        CardID UUID PRIMARY KEY,
+        RenterID UUID REFERENCES renter (RenterID),
+        AddressID UUID REFERENCES locations (LocationID),
+        CardNumber VARCHAR(16) NOT NULL,
+        ExpirationDate DATE NOT NULL,
+        CVV VARCHAR(4) NOT NULL
+    );
+CREATE TABLE
+    booking (
+        BookingID UUID PRIMARY KEY,
+        CardID UUID REFERENCES card (CardID),
+        RenterID UUID REFERENCES renter (RenterID),
+        AgentID UUID REFERENCES agent (AgentID),
+        PropertyID UUID NOT NULL,
+        StartDate DATE NOT NULL,
+        EndDate DATE NOT NULL
+    );
+CREATE TABLE
+    apartment (
+        ApartmentID UUID PRIMARY KEY,
+        PropertyID UUID REFERENCES property (PropertyID),
+        BuildingType VARCHAR(20) NOT NULL,
+        Floor INT NOT NULL,
+        NumRooms INT NOT NULL
+    );
