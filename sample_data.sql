b6da08873aaa64d5ac5bc2e1fb51245534b120bc
Hawktseibert <tseibert@hawk.iit.edu>
2025-11-07 11:03:14 -0600

    new sample data branch, added more data

--- sample_data.sql
+++ sample_data.sql
@@ -1,18 +1,5 @@
--- Table creation
-CREATE TABLE Department (
-    Dept_ID SERIAL PRIMARY KEY,
-    Dept_Name VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE Employee (
-    Emp_ID SERIAL PRIMARY KEY,
-    Fname VARCHAR(30),
-    Lname VARCHAR(30),
-    Salary NUMERIC(10,2),
-    Dept_ID INT REFERENCES Department(Dept_ID)
-);
-
 -- Sample data
+-------------------------------------User & Location Data-----------------------------------------------
 INSERT INTO Users (User_ID, User_Name, Email, User_Type) 
 VALUES ('UUID','Tim','timEmail@email.com','Agent'),
 	   ('UUID','Aidan','aidanEmail@email.com','Agent'),
@@ -21,6 +8,7 @@ VALUES ('UUID','Tim','timEmail@email.com','Agent'),
 	   
 INSERT INTO Users_Addresses (User_Address_ID, User_ID, Address_ID) 
 VALUES ('UUID','UUID_Tim','UUID_Address_Tim'),
+	   ('UUID','UUID_Aidan','UUID_Address_Aidan'),
 	   ('UUID','UUID_Yousef','UUID_Address_Yousef'),
 	   ('UUID','UUID_Emily','UUID_Address_Emily'),
 	   ('UUID','UUID_Emily','UUID_Address_Emily1');
@@ -32,6 +20,27 @@ VALUES ('UUID','UUID_Tim','Senior Agent','Star Realtors','3168565619'),
 INSERT INTO Renters (Renter_ID, User_ID, Move_In_Date, Pref_Location, Budget) 
 VALUES ('UUID','UUID_Yousef','2025-12-01','Chicago','5000'),
 	   ('UUID','UUID_Emily','2025-12-02','St. Louis','3200');
+
+INSERT INTO Card (Card_ID, Renter_ID, Address_ID, Card_Number, Expiration) 
+VALUES ('UUID','UUID_Renter_Yousef','UUID_Address_Yousef','1234678221234123','05/28'),
+	   ('UUID','UUID_Renter_Emily','UUID_Address_Emily','1334678221234123','07/29'),
+	   ('UUID','UUID_Renter_Emily','UUID_Address_Emily1','1434678221234123','06/26');
+
+INSERT INTO Locations (Location_ID, Address, City, State_, Country, Zip_Code) 
+VALUES ('UUID','3303 s State st','Chicago','IL','United States','60616'),
+	   ('UUID','3304 n State st','Chicago','IL','United States','60616'),
+	   ('UUID','12678 Honeygrove ct','St. Louis','MO','United States','63146'),
+	   ('UUID','2301 s State st','Chicago','IL','United States','60616'),
+	   ('UUID','12768 Honeygrove ct','St. Louis','MO','United States','63146');
+	   
+-------------------------------------Property Data-----------------------------------------------
+
+INSERT INTO Property (Property_ID, Type_, Location_ID, Description, Price, Availability, Crime_Rate) 
+VALUES ('UUID','3303 s State st','Chicago','IL','United States','60616'),
+	   ('UUID','3304 n State st','Chicago','IL','United States','60616'),
+	   ('UUID','12678 Honeygrove ct','St. Louis','MO','United States','63146'),
+	   ('UUID','2301 s State st','Chicago','IL','United States','60616'),
+	   ('UUID','12768 Honeygrove ct','St. Louis','MO','United States','63146');
 
 
 
