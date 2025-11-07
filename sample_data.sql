ab37070c0ae69ae4e3c0440be4a940c01f556c69
Hawktseibert <tseibert@hawk.iit.edu>
2025-11-07 11:33:00 -0600

    sample data added, need to create UUIDs for all relations

--- sample_data.sql
+++ sample_data.sql
@@ -36,11 +36,53 @@ VALUES ('UUID','3303 s State st','Chicago','IL','United States','60616'),
 -------------------------------------Property Data-----------------------------------------------
 
 INSERT INTO Property (Property_ID, Type_, Location_ID, Description, Price, Availability, Crime_Rate) 
-VALUES ('UUID','3303 s State st','Chicago','IL','United States','60616'),
-	   ('UUID','3304 n State st','Chicago','IL','United States','60616'),
-	   ('UUID','12678 Honeygrove ct','St. Louis','MO','United States','63146'),
-	   ('UUID','2301 s State st','Chicago','IL','United States','60616'),
-	   ('UUID','12768 Honeygrove ct','St. Louis','MO','United States','63146');
+VALUES ('UUID','House','UUID_Location','A fun place to live','400000','1','0.001'),
+	   ('UUID','House','UUID_Location','A fun place to live','500000','1','0.0056'),
+	   ('UUID','House','UUID_Location','A fun place to live','1000000','1','0.002'),
+	   ('UUID','Apartment','UUID_Location','A fun place to rent','5000','1','0.001'),
+	   ('UUID','Apartment','UUID_Location','A fun place to rent','750','0','0.1'),
+	   ('UUID','Apartment','UUID_Location','A fun place to rent','1500','1','0.045'),
+	   ('UUID','Commercial Building','UUID_Location','A fun place to work','55000','1','0.0001'),
+	   ('UUID','Land','UUID_Location','A fun place to build','2000000','1','0.00001'),
+	   ('UUID','Vacation Home','UUID_Location','A fun place to relax','5000000','0','0.000001'),
+	   
+INSERT INTO Booking (Booking_ID, Card_ID, Property_ID, Renter_ID, Agent_ID, Start_Date, End_Date) 
+VALUES ('UUID','UUID_Card','UUID_Property','UUID_Agent_Tim','UUID_Renter_Emily','2026-03-12','2027-3-15'),
+	   ('UUID','UUID_Card','UUID_Property','UUID_Agent_Tim','UUID_Renter_Emily','2026-06-12','2027-6-11'),
+	   ('UUID','UUID_Card','UUID_Property','UUID_Agent_Aidan','UUID_Renter_Yousef','2026-09-12','2027-9-10');
+
+INSERT INTO House (House_ID, Property_ID, Num_Rooms, Sq_Footage) 
+VALUES ('UUID','UUID_Property','3','1900'),
+	   ('UUID','UUID_Property','4','2100'),
+	   ('UUID','UUID_Property','7','4000');
+
+INSERT INTO Apartment (Apartment_ID, Property_ID, Num_Rooms, Sq_Footage, Building_Type) 
+VALUES ('UUID','UUID_Property','2','750','Studio'),
+	   ('UUID','UUID_Property','3','1500','Condo'),
+	   ('UUID','UUID_Property','5','3000','Penthouse');
+
+INSERT INTO Commercial_Buildings (Commercial_ID, Property_ID, Sq_Footage, Business_Type) 
+VALUES ('UUID','UUID_Property','35000','Bank'),
+	   ('UUID','UUID_Property','3','27000','Law Firm'),
+	   ('UUID','UUID_Property','5','30000','Hardware Store');
+
+INSERT INTO Land (Land_ID, Property_ID) 
+VALUES ('UUID','UUID_Property'),
+	   ('UUID','UUID_Property'),
+	   ('UUID','UUID_Property');
+
+INSERT INTO Vacation_Home (Vacation_ID, Property_ID) 
+VALUES ('UUID','UUID_Property'),
+	   ('UUID','UUID_Property'),
+	   ('UUID','UUID_Property');
+	   
+INSERT INTO School (School_ID, Property_ID, School_Name, School_Address) 
+VALUES ('UUID','UUID_Property','Parkway North','106 S FeeFee ln'),
+	   ('UUID','UUID_Property','Parkway South','66 N FeeFee dr'),
+	   ('UUID','UUID_Property','Parkway East','56 S FeeFee dr');
+
+
+		
 
 
 
